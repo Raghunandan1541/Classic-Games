04a993d19e7da60253f6d937d0230b9941f1c786
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-03-29 10:16:18 +0530

    Combined Player.js with canvas.js and added projectile functionality

--- shooting/js/canvas.js
+++ shooting/js/canvas.js
@@ -1,13 +1,94 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-width = canvas.width = window.innerWidth 
-height = canvas.height = window.innerHeight 
+canvas.width = window.innerWidth 
+canvas.height = window.innerHeight 
 
-ctx.fillRect(0, 0, width, height)
+
+class Player {
+	constructor(x, y, radius, color) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+	}
+
+	draw = () => {
+		
+		ctx.beginPath()
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		ctx.fillStyle = this.color
+		ctx.fill()
+	}
+}
+
+class Projectile {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+	}
+	
+	draw = () => {
+		
+		ctx.beginPath()
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		ctx.fillStyle = this.color
+		ctx.fill()
+	}
+
+	update = () => {
+		this.draw()
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+	}
+}
+
 
 var x = canvas.width / 2
 var y = canvas.height / 2
 let player = new Player(x, y, 10, 'white')
+let projectiles = []
 
-player.draw();
+
+
+function animate() {
+	animationId = requestAnimationFrame(animate)
+	ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'
+	ctx.fillRect(0, 0, canvas.width, canvas.height)
+	player.draw()
+
+
+	projectiles.forEach((projectile, index) => {
+		projectile.update()
+
+		if(
+			projectile.x + projectile.radius < 0 ||
+			projectile.x - projectile.radius > canvas.width ||
+			projectile.y + projectile.radius < 0 ||
+			projectile.y - projectile.radius > canvas.height
+		) {
+			setTimeout(() => {
+				projectiles.splice(index, 1)
+			}, 0)
+		}
+	})
+
+}
+
+addEventListener('click', (event) => {
+	const angle = Math.atan2(
+		event.clientY - canvas.height / 2, 
+		event.clientX - canvas.width / 2
+		)
+
+	const velocity = {
+		x: Math.cos(angle) * 5,
+		y: Math.sin(angle) * 5
+	}
+	projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity))
+})
+
+animate()
